f12cf2046ea2babd3047f01dbd0e109c7a597418
victor95 <victor95@victorpalomocastro@gmail.com>
2015-08-06 02:02:31 -0300

    create the class it will search add_breadcrumb on all controllers

--- lib/breadcrumbs_on_rails_to_gretel/search_old_breadcrumbs.rb
+++ lib/breadcrumbs_on_rails_to_gretel/search_old_breadcrumbs.rb
@@ -1,17 +1,28 @@
 module BreadcrumbsOnRailsToGretel
 	class SearchOldBreadcrumbs
-		attr_accessor :breadcrumbs, :breadcrumbs_controller, :parser, :ruby2ruby, :controller_sexp, :klass
+		require 'ruby2ruby'
+		require 'ruby_parser'
+		attr_accessor :breadcrumbs, :breadcrumbs_root, :breadcrumbs_controller, :parser, :ruby2ruby, :controller_sexp, :klass
 
-		def initialize(path: %w(app controllers))
-			self.parser      = RubyParser.new
-			self.ruby2ruby   = Ruby2Ruby.new
-			self.breadcrumbs = []
+		def initialize
+			self.parser           = ::RubyParser.new
+			self.ruby2ruby        = ::Ruby2Ruby.new
+			self.breadcrumbs      = {}
+			self.breadcrumbs_root = []
+		end
 
-			Rails.root.join(*path).each_child do |file| 
+		def look_all_controllers
+			Rails.root.join('app', 'controllers').each_child do |file| 
 				prepare_controller(file)
 				search_on_class_methods
-				search_on_actions
-				breadcrumbs[klass] << breadcrumbs_controller
+
+				if klass == ApplicationController
+					breadcrumbs_root << breadcrumbs_controller
+				else
+					search_on_actions
+					breadcrumbs[klass] ||= []
+					breadcrumbs[klass] << breadcrumbs_controller
+				end
 			end
 		end
 
@@ -25,42 +36,35 @@ module BreadcrumbsOnRailsToGretel
 		end
 
 		def search_on_class_methods
-			controller_sexp.find_nodes(:call).select {|sexp| /(before_(?:action|filter)|add_breadcrumb)/ === method_name(sexp) }.each do |sexp|
+			controller_sexp.find_nodes(:call).select { |sexp| [:add_breadcrumb, BEFORE].include? method_name(sexp) }.each do |sexp|
 				
 				options = get_callback_options(sexp)
 
 				if method_name(sexp) == :add_breadcrumb
 					add_breacrumbs(sexp, options)
 				else
-
-
+					# Check before_action
 				end			
 			end
 		end
 
-		def search_on_actions
-			
-		end
-
-		checkbreadcrumb
-
-		def add_breacrumbs(sexp, only: nil, except: nil, **conditionals) 
+		def add_breacrumbs(sexp, only: [], except: [], **conditionals) 
 			actions = select_with_only_except(controller_actions, only, except)
-			actions.each { |action| add_breacrumb(sexp, action, callback_ops) }
+			actions.each { |action| add_breacrumb(sexp, action, conditionals: conditionals) }
 		end
 
-		def add_breacrumb(sexp, action, conditionals)
-			source = remove_procs(convert_to_source(sexp).gsub('view_context.', '')) 
-			
-			source.scan(/add_breadcrumb\((".+?"),?\s?[:']?(.+)?/) do |name, path| 
-				breadcrumbs_controller[action] ||= []
-				breadcrumbs_controller[action] << {name: name, path: path.chop, view: nil }.merge conditionals]
-				breadcrumbs_controller[action] << {name: name, path: path.chop, view: nil }.merge conditionals]
-			end
+		def add_breacrumb(sexp, action, conditionals: {})
+			array_args = convert_to_source(sexp).gsub('add_breadcrumb(', '').gsub('\'', '').split(', ')[0..1]
+
+			breadcrumbs_controller[action] ||= []
+			breadcrumbs_controller[action] << { name: array_args[0], path: array_args[1], view: nil }.merge(conditionals)
 		end
 
 		def remove_procs(source)
-			source.gsub(/(?:lambda|Proc\.new|proc) { \|(\w)\| ("?.+?) }/) { $2.gsub("#{$1}.", '') }
+			source.gsub(/(?:lambda|Proc\.new|proc) { \|(\w+)\| (\(*"?.+?) }\)?/) do
+				formated = $2.gsub("#{$1}.", '')
+				"'#{formated}'"
+			end
 		end
 
 		def get_callback_options(sexp)
@@ -73,22 +77,22 @@ module BreadcrumbsOnRailsToGretel
 			end
 
 			#Search for if and unless
-			remove_procs(source).scan(/:(if|unless) => [:"](.+?)[",\s]/) do |type, block|
-				options[type.to_sym] = block
+			source.scan(/:(if|unless) => (?:\('(.+?)'|:(.+?)[\),])/) do |type, proc, symbol|
+				options[type.to_sym] = proc || symbol
 			end
 
 			options
 		end
 
 		def convert_to_source(sexp)
-			ruby2ruby.process(sexp.deep_clone)
+			remove_procs(ruby2ruby.process(sexp.deep_clone).gsub('view_context.', ''))
 		end
 
 		def controller_actions
 			klass.action_methods.reject{|action| action.start_with? '_' }
 		end
 
-		def select_with_only_except(collection, only=[], except=[])
+		def select_with_only_except(collection, only, except)
 			collection.select { |element| (only.empty? || only.include?(element)) && except.exclude?(element) }
 		end
 
