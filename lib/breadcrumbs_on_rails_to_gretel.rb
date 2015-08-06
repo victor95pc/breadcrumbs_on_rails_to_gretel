6e67aaaad604160e0a50929dbf39996ec8de9919
victor95 <victor95@victorpalomocastro@gmail.com>
2015-08-06 02:00:24 -0300

    Get before type of callback looking on Rails version

--- lib/breadcrumbs_on_rails_to_gretel.rb
+++ lib/breadcrumbs_on_rails_to_gretel.rb
@@ -1,4 +1,9 @@
 module BreadcrumbsOnRailsToGretel
 	require "breadcrumbs_on_rails_to_gretel/railtie.rb"
 	require "breadcrumbs_on_rails_to_gretel/search_old_breadcrumbs.rb"
+	if Rails::VERSION::MAJOR >= 4
+		BEFORE = :before_action
+	else
+		BEFORE = :before_filter
+	end
 end
